d3e198fc29da4665234ebbda9d2f507fdd79ca5b
hannielnguyen <99895301+hannielnguyen@users.noreply.github.com>
2022-04-24 04:40:11 +0700

    Update HWH42.cpp

--- HWH42.cpp
+++ HWH42.cpp
@@ -1,71 +1,49 @@
-#include<iostream>
+//Вводятся размеры двумерного массива и его элементы. Вывести сумму положительных элементов массива
+
+#include <iostream>
 using namespace std;
-void enterarray(int*& a, int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << "enter a[" << i << "]" << endl;
-		cin >> a[i];
-	}
-}
-void outputarray(int*& a, int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << " " << a[i];
-	}
-}
- 
-void permutation(int& a, int& b)
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
-void sortdescending(int*& a, int n)
-{
-	for (int i = 0; i < n - 1; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (a[i] < a[j])
-			{
-				permutation(a[i], a[j]);
-			}
+
+void input(int** a, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
 		}
- 
 	}
 }
-void Sortupascending(int*& a, int n)
-{
-	for (int i = 0; i < n - 1; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (a[i] > a[j])
-			{
-				permutation(a[i], a[j]);
-			}
+
+int Sum(int** a, int m, int n) {
+	int sum = 0;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (a[i][j] > 0) sum += a[i][j];
 		}
- 
+	}
+	return sum;
+}
+
+void output(int** a, int m, int n) {
+	cout << "Array:" << endl;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
 	}
 }
-int main()
-{
-	int n;
-	cout << "enter n:";
-	cin >> n;
-	int* a = new int[n];
-	enterarray(a, n);
-	outputarray(a, n);
-	cout << endl;
-	cout << "sorted array descending:";
-	sortdescending(a, n);
-	outputarray(a, n);
-	cout << endl;
-	cout << "Sort up ascending:";
-	Sortupascending(a, n);
-	outputarray(a, n);
+
+int main() {
+	int m, n;
+	cout << "Enter (m, n) :";
+	cin >> m >> n;
+	int** a = new int* [m];
+	for (int i = 0; i < m; i++) {
+		a[i] = new int[n];
+	}
+	input(a, m, n);
+	output(a, m, n);
+	cout << "Sum of the positive elements of an array = " << Sum(a, m, n);
+	for (int i = 0; i < m; i++) {
+		delete[]a[i];
+	}
 	delete[]a;
-	
 }
