0ef08c1603b923a3dbbde101bc0d3ffcdba0242d
hannielnguyen <99895301+hannielnguyen@users.noreply.github.com>
2022-05-25 03:12:09 +0700

    HW10.cpp

--- HW10.cpp
+++ HW10.cpp
@@ -1,65 +1,61 @@
 #include <iostream>
 using namespace std;
 
-class Tree {
+struct Tree {
 	int data;
-	Tree* left, * right;
-public:
-	Tree() {
-		data = 0;
-		left = right = NULL;
-	}
+	Tree* left;
+	Tree* right;
 	Tree(int val) {
 		data = val;
 		left = right = NULL;
 	}
-	Tree* Insert(Tree* root, int val) {
-		if (!root) {
-			return new Tree(val);
-		}
-		if (val > root->data) {
-			root->right = Insert(root->right, val);
-		}
-		else {
-			root->left = Insert(root->left, val);
-		}
-		return root;
-
-	}
-	void Ascending(Tree* root) {
-		if (!root) {
-			return;
-		}
-		Ascending(root->left);
-		cout << root->data << endl;
-		Ascending(root->right);
-	}
-	void Decending(Tree* root) {
-		if (!root) {
-			return;
-		}
-		Decending(root->right);
-		cout << root->data << endl;
-		Decending(root->left);
-	}
 };
+void Insert(Tree*& root, int val) {
+	if (!root) {
+		root = new Tree(val);
+		return;
+	}
+	if (val > root->data) {
+		Insert(root->right, val);
+	}
+	else {
+		Insert(root->left, val);
+	}
+}
+
+void Ascending(Tree*& root) {
+	if (!root) {
+		return;
+	}
+	Ascending(root->left);
+	cout << root->data << endl;
+	Ascending(root->right);
+}
+
+void Decending(Tree*& root) {
+	if (!root) {
+		return;
+	}
+	Decending(root->right);
+	cout << root->data << endl;
+	Decending(root->left);
+}
 
 int main() {
-	Tree tree;
-	Tree * root = NULL;
-	root = tree.Insert(root, 8);
-	tree.Insert(root, 9);
-	tree.Insert(root, 4);
-	tree.Insert(root, 7);
-	tree.Insert(root, 11);
-	tree.Insert(root, 5);
-	tree.Insert(root, 10);
-	tree.Insert(root, 6);
-
+	Tree* root = NULL;
+	Insert(root, 8);
+	Insert(root, 9);
+	Insert(root, 4);
+	Insert(root, 7);
+	Insert(root, 11);
+	Insert(root, 5);
+	Insert(root, 10);
+	Insert(root, 6);
 	cout << "Output:" << endl;
 	cout << "Ascending:" << endl;
-	tree.Ascending(root);
+	Ascending(root);
 	cout << "Decending:" << endl;
-	tree.Decending(root);
+	Decending(root);
 	return 0;
 }
+
